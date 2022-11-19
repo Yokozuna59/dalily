fc5e432f6794d1042b8518cbc2d0f8f78394709d
Yokozuna59 <u.yokozuna@gmail.com>
2022-11-19 13:58:42 +0300

    feat: when user input valid navigate to MyHomePage
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

--- lib/auth.dart
+++ lib/auth.dart
@@ -6,7 +6,6 @@ Future<bool> singin(String emailAddress, String password) async {
         .signInWithEmailAndPassword(email: emailAddress, password: password);
     return true;
   } on FirebaseAuthException catch (e) {
-    print(e.message);
     return false;
   }
 }
