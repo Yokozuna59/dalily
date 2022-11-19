7da5a864c55cf44c7f27ba8c509562f53854717c
pxooo <waleedstu@gmail.com>
2022-11-19 12:16:44 +0300

    Add the submit button to the username page

--- lib/pages/username_page.dart
+++ lib/pages/username_page.dart
@@ -16,7 +16,8 @@ class Username_Page extends StatefulWidget {
 // ignore: camel_case_types
 class _Username_PageState extends State<Username_Page> {
   final myController = TextEditingController();
-
+  late String password;
+  late String email;
   @override
   void dispose() {
     myController.dispose();
@@ -39,64 +40,89 @@ class _Username_PageState extends State<Username_Page> {
     final height = size.height;
     final width = size.width;
     return Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: KbackgroundColor,
         body: Column(children: [
           SizedBox(
             height: height * 0.04,
           ),
-          Stack(
-            children: <Widget>[
-              SizedBox(
-                width: width,
-                child: Image.asset(
-                  "lib/assets/map_background.jpg",
-                  fit: BoxFit.fitWidth,
-                  opacity: const AlwaysStoppedAnimation(0.05),
+          Flexible(
+            child: Stack(
+              children: <Widget>[
+                SizedBox(
+                  width: width,
+                  child: Image.asset(
+                    "lib/assets/map_background.jpg",
+                    fit: BoxFit.fitWidth,
+                    opacity: const AlwaysStoppedAnimation(0.05),
+                  ),
                 ),
-              ),
-              Image.asset("lib/assets/logo-removebg-preview.png")
-            ],
+                Image.asset("lib/assets/logo-removebg-preview.png")
+              ],
+            ),
           ),
-          Column(children: <Widget>[
-            SizedBox(
-              height: height * 0.008,
-            ),
-            SizedBox(
-              width: width * 0.7,
-              child: TextField(
-                controller: myController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                  ),
-                  hintText: 'Email: example@gmail.com',
-                ),
-                onChanged: (text) {
-                  print('First text field: $text');
-                },
+          SingleChildScrollView(
+            child: Column(children: <Widget>[
+              SizedBox(
+                height: height * 0.008,
               ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            SizedBox(
-              width: width * 0.7,
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              SizedBox(
+                width: width * 0.7,
+                child: TextField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    hintText: 'Email: example@gmail.com',
                   ),
-                  hintText: 'Password',
+                  onChanged: (text) {
+                    email = text;
+                    print('First text field: $text');
+                  },
                 ),
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                onChanged: (text) {
-                  print('First text field: $text');
-                },
               ),
-            ),
-          ]),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              SizedBox(
+                width: width * 0.7,
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    hintText: 'Password',
+                  ),
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  onChanged: (text) {
+                    password = text;
+                    print('Second text field: $text');
+                  },
+                ),
+              ),
+              SizedBox(height: height * 0.04),
+              SizedBox(
+                height: height * 0.06,
+                width: width * 0.4,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF3988C9),
+                      shadowColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Submit",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    )),
+              ),
+            ]),
+          ),
         ]));
   }
 }
